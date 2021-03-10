e039db48452816f160030c75b8f89807322e503d
Rupus Reinefjord <rupus@reinefjord.net>
2021-03-10 22:59:19 +0100

    Writing to csv works

--- pass2csv.nim
+++ pass2csv.nim
@@ -1,4 +1,4 @@
-import os, osproc, parseopt, re, strformat, strutils
+import os, osproc, parseopt, re, strformat, strutils, tables
 
 type
   GetField = object
@@ -9,7 +9,7 @@ type
     group: string
     title: string
     password: string
-    fields: seq[tuple[fieldName: string, value: string]]
+    fields: Table[string, string]
     notes: string
 
 proc setMeta(entry: var EntryData; path, groupingBase: string) =
@@ -32,42 +32,61 @@ proc setData(entry: var EntryData; data: string; exclude: seq[Regex];
     for i, line in tail:
       let match = line.findBounds(field.pattern)
       if match != (-1, 0):
-        let
-          inverseMatch = line[0 ..< match[0]] & line[match[1] + 1 .. ^1]
-          value = strutils.strip(inverseMatch)
-          fieldMatch = (fieldName: field.name, value: value)
-        entry.fields.add(fieldMatch)
+        let inverseMatch = line[0 ..< match[0]] & line[match[1] + 1 .. ^1]
+        let value = strutils.strip(inverseMatch)
+        entry.fields[field.name] = value
         tail.delete(i)
         break
-  entry.notes = tail.join("\n")
+  entry.notes = tail.join("\n").strip()
 
 proc decrypt(gpgBinary, filename: string): tuple[output: string, exitCode: int] =
   let cmd = &"{gpgBinary} --decrypt --quiet {quoteShell(filename)}"
   return execCmdEx(cmd)
 
+proc write(outFile: string; entries: seq[EntryData]; getFields: seq[GetField]) =
+  var file: File
+  if outFile == "-":
+    file = stdout
+  else:
+    file = open(outFile, fmWrite)
+  for entry in entries:
+    var fields: seq[string]
+    for field in getFields:
+      fields.add(entry.fields.getOrDefault(field.name))
+    var columns = @[entry.group, entry.title, entry.password] & fields & @[entry.notes]
+    for i, col in columns:
+      if col.contains({',', '\n', '"'}):
+        let quotesEscaped = col.replace("\"", "\"\"")
+        columns[i] = &"\"{quotesEscaped}\""
+    let row = columns.join(",")
+    file.writeLine(row)
+
 proc main(storePath, groupingBase, gpgBinary, outFile: string;
           exclude: seq[Regex]; getFields: seq[GetField]) =
+  var entries: seq[EntryData]
   var failures: seq[string]
   for path in walkDirRec(storePath, relative = true):
     if path.startsWith(".git"):
       continue
     if not path.endsWith(".gpg"):
       continue
-    echo("Processing " & path)
+    writeLine(stderr, "Processing " & path)
     let (output, exitCode) = decrypt(gpgBinary, joinPath(storePath, path))
     if exitCode != 0:
-      echo(&"{gpgBinary} exited with code {exitCode}:")
-      echo(output)
+      writeLine(stderr, &"{gpgBinary} exited with code {exitCode}:")
+      writeLine(stderr, output)
       failures.add(path)
       continue
     var entry: EntryData
     entry.setMeta(joinPath(storePath, path), groupingBase)
     entry.setData(output, exclude, getFields)
-    echo(entry)
+    writeLine(stderr, entry)
+    entries.add(entry)
   if failures.len() > 0:
-    echo("Failed to decrypt: ")
+    writeLine(stderr, "Failed to decrypt: ")
     for failed in failures:
-      echo(failed)
+      writeLine(stderr, failed)
+  outFile.write(entries, getFields)
 
 when isMainModule:
   var
@@ -81,7 +100,7 @@ when isMainModule:
     case kind
     of cmdArgument:
       if storePath != "":
-        echo(&"Unexpected argument '{key}', only one path can be given.")
+        writeLine(stderr, &"Unexpected argument '{key}', only one path can be given.")
         quit(1)
       storePath = key.expandTilde().normalizedPath()
     of cmdLongOption, cmdShortOption:
@@ -98,18 +117,18 @@ when isMainModule:
         if key.startsWith("get-"):
           let fieldName = key[4 .. ^1]
           if val == "":
-            echo(&"Missing a pattern for field '{fieldName}'.")
+            writeLine(stderr, &"Missing a pattern for field '{fieldName}'.")
             quit(1)
           let pattern = re(val, flags = {reStudy, reIgnoreCase})
           let field = GetField(name: fieldName, pattern: pattern)
           getFields.add(field)
         else:
-          echo(&"Unknown argument '{key}'.")
+          writeLine(stderr, &"Unknown argument '{key}'.")
           quit(1)
     of cmdEnd:
       assert(false)
   if storePath == "":
-    echo("Please provide a path to your password store.")
+    writeLine(stderr, "Please provide a path to your password store.")
     quit(1)
   if groupingBase == "":
     groupingBase = storePath
